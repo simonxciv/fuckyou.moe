db8cf038fa45f46307a9a92853c602eb0ce40da9
Simon Buckley <hello@simonbuckley.com.au>
2022-02-15 19:27:22 +1100

    Add error template, move styles to global stylesheet

---
+++ src/routes/__error.svelte
@@ -0,0 +1,27 @@
+<script context="module">
+	/** @type {import('@sveltejs/kit').ErrorLoad} */
+	export function load({ error, status }) {
+		return {
+			props: {
+                status: status,
+				title: error.message
+			}
+		};
+	}
+</script>
+
+<script>
+	export let status;
+    export let title;
+</script>
+
+<h1>{status}<span>fucking idiot</span></h1>
+
+<style>
+    span {
+        font-size: 0.35em;
+        display: block;
+        font-family: 'Courier New', Courier, monospace;
+        font-weight: bold;
+    }
+</style>
